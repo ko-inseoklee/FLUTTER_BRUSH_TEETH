9e06af3cc1541f1a6dcef63c1e8e0e9b5d6ae7a1
Lee In Seok <tjrkd222@gmail.com>
2022-09-26 12:55:49 +0900

    1페이지 횟수 조절 다이얼로그 완료.
    remain
     - 디테일
     - 카메라 모듈
     - 빌드

--- lib/home/HomeTab.dart
+++ lib/home/HomeTab.dart
@@ -142,7 +142,7 @@ class _HomeTabState extends State<HomeTab> {
                           child: Image.asset('images/home/timeRed.png', color: Color(isClean? 0xff263FE9 :0xffFF8C5B),)
                       ),
                       Container(
-                        margin: EdgeInsets.only(right: isClean? 18: 0),
+                        margin: EdgeInsets.only(right: 18),
                         child: Text(isClean?"0h 경과":'9h 경과',
                           style: TextStyle(
                               color: Color(isClean? 0xff263FE9 :0xffFF8C5B),
@@ -229,8 +229,88 @@ class _HomeTabState extends State<HomeTab> {
             child: Text("3", style: TextStyle(color: Color(0xffFF8C5B),fontSize: 13.0),),
           ),
           Container(
-            margin: EdgeInsets.only(left: 21),
-            child: Image.asset('images/home/brushCountButton.png',color: Color(0xff424242),),
+            margin: EdgeInsets.only(left: 22), width: 14, height: 10,
+            child: FlatButton(
+              padding: EdgeInsets.zero,
+              onPressed: () {
+                showDialog(context: context,
+                    barrierDismissible: true,
+                    builder: (context){
+                      int tempCnt = 3;
+                  return StatefulBuilder(builder: (context,StateSetter setDialogState){
+                    return Dialog(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(26)
+                      ),
+                      child: Container(
+                        width: 201, height: 262,
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 141, height: 36, margin: EdgeInsets.only(top: 19),
+                              child: Text('오늘의 목표 양치횟수',style: TextStyle(fontSize: 17, fontWeight: FontWeight.w700, color: Color(0xff4860FF)),),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  width: 40, height: 36, margin: EdgeInsets.only(top: 40),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(8),
+                                    border: Border.all(color: Color(0xffEAEAEA), width: 1)
+                                  ),
+                                  child: FlatButton(
+                                    padding: EdgeInsets.zero,
+                                    onPressed: () {
+                                      setDialogState((){
+                                        tempCnt--;
+                                      });
+                                    }, child: Icon(CupertinoIcons.chevron_down,size: 14,),
+
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(top: 40, left: 15, right: 15), width: 60, alignment: Alignment.center,
+                                  child: Text(tempCnt.toString(), style: TextStyle(fontSize: 36, fontWeight: FontWeight.w400, color: Color(0xff4860FF)),),
+                                ),
+                                Container(
+                                  width: 40, height: 36, margin: EdgeInsets.only(top: 40),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(8),
+                                      border: Border.all(color: Color(0xffEAEAEA), width: 1)
+                                  ),
+                                  child: FlatButton(
+                                    padding: EdgeInsets.zero,
+                                    onPressed: () {
+                                      setState(() {
+                                        setDialogState((){
+                                          tempCnt++;
+                                          print(tempCnt);
+                                        });
+                                      });
+                                    }, child: Icon(CupertinoIcons.chevron_up,size: 14,),
+
+                                  ),
+                                )
+                              ],
+                            ),
+                            Container(
+                              width: 90, height: 44, margin: EdgeInsets.only(top: 42), alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(26),
+                                color: Color(0xff7385FF)
+                              ),
+                              child: FlatButton(onPressed: () => Navigator.pop(context),
+                              child: Text('확인', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400, color: Color(0xffFFFFFF)),)),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  });
+                });
+              },
+              child: Image.asset('images/home/brushCountButton.png',color: Color(0xff424242),)),
           ),
         ],
       ),
