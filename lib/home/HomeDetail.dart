9466b2cb46694749dfac76e039ac44e0ce6020b5
Lee In Seok <tjrkd222@gmail.com>
2022-09-26 21:56:21 +0900

    1페이지 디테일 작업 완료

--- lib/home/HomeDetail.dart
+++ lib/home/HomeDetail.dart
@@ -24,8 +24,7 @@ class _HomeDetailPageState extends State<HomeDetailPage> {
 
       ),
       backgroundColor: Color(0xffFFFFFF),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: ListView(
         children: [
           Container(
             width: 108, height: 29, margin: EdgeInsets.only(left: 20, top: 20),
@@ -120,7 +119,6 @@ class _HomeDetailPageState extends State<HomeDetailPage> {
           ),
           afterCamera?
               Container(
-                height: 530,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -149,7 +147,237 @@ class _HomeDetailPageState extends State<HomeDetailPage> {
                       child: Text('치태잔여율 1 결과', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400, color: Color(0xff434343)),),
                     ),
                     firstExpanded?
-                        Container() :
+                        Container(
+                          child: Column(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(top: 15),
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      width: 67, height: 26, margin: EdgeInsets.only(left: 23,top: 15), alignment: Alignment.center,
+                                      child: Text('치태 잔여율', style: TextStyle(color: Color(0xffFF7E47), fontSize: 12),),
+                                    ),
+                                    Container(
+                                      width: 67, height: 26, margin: EdgeInsets.only(left: 200,top: 15), alignment: Alignment.centerRight,
+                                      child: Text('치태 제거율', style: TextStyle(color: Color(0xff6377F9), fontSize: 12),),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Container(
+                                    width: 80, height: 38, margin: EdgeInsets.only(left: 23), alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                        color: Color(0xffFFE6DC),
+                                        borderRadius: BorderRadius.circular(15)
+                                    ),
+                                    child: Text('20%', style: TextStyle(color: Color(0xffFF7E47), fontSize: 15, fontWeight: FontWeight.w700),),
+                                  ),
+                                  Container(
+                                    width: 259, height: 38, margin: EdgeInsets.only(left: 2), alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                        color: Color(0xff6377F9),
+                                        borderRadius: BorderRadius.circular(15)
+                                    ),
+                                    child: Text('80%', style: TextStyle(color: Color(0xffFFFFFF), fontSize: 15, fontWeight: FontWeight.w700),),
+                                  ),
+                                ],
+                              ),
+                              Container(
+                                width: 347, height: 77, margin: EdgeInsets.only(top: 12, left: 23, right: 23),
+                                decoration: BoxDecoration(
+                                    color: Color(0xffFFE6DC),
+                                    borderRadius: BorderRadius.circular(15)
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Container(
+                                          width: 39, height: 19, alignment: Alignment.bottomLeft, margin: EdgeInsets.only(top: 4.5,left: 66),
+                                          child: Text('앞니', style: TextStyle(color: Color(0xffFF7E47), fontSize: 14),),
+                                        ),
+                                        Container(
+                                          width: 53, height: 6, margin: EdgeInsets.only(left: 9, top: 5),
+                                          decoration: BoxDecoration(
+                                              color: Color(0xFFFF7E47),
+                                              borderRadius: BorderRadius.circular(15)
+                                          ),
+                                        ),
+                                        Container(
+                                          width: 39, height: 19, alignment: Alignment.bottomLeft, margin: EdgeInsets.only(top: 4.5,left: 10),
+                                          child: Text('5%', style: TextStyle(color: Color(0xffFF7E47), fontSize: 14),),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        Container(
+                                          width: 39, height: 26, alignment: Alignment.centerLeft, margin: EdgeInsets.only(left: 66),
+                                          child: Text('아랫니', style: TextStyle(color: Color(0xffFF7E47), fontSize: 14),),
+                                        ),
+                                        Container(
+                                          width: 90, height: 6, margin: EdgeInsets.only(left: 9, top: 5),
+                                          decoration: BoxDecoration(
+                                              color: Color(0xFFFF7E47),
+                                              borderRadius: BorderRadius.circular(15)
+                                          ),
+                                        ),
+                                        Container(
+                                          width: 39, height: 26, alignment: Alignment.centerLeft, margin: EdgeInsets.only(left: 10),
+                                          child: Text('12%', style: TextStyle(color: Color(0xffFF7E47), fontSize: 14),),
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        Container(
+                                          width: 39, height: 19, alignment: Alignment.bottomLeft, margin: EdgeInsets.only(left: 66),
+                                          child: Text('윗니', style: TextStyle(color: Color(0xffFF7E47), fontSize: 14),),
+                                        ),
+                                        Container(
+                                          width: 27, height: 6, margin: EdgeInsets.only(left: 9, top: 5),
+                                          decoration: BoxDecoration(
+                                              color: Color(0xFFFF7E47),
+                                              borderRadius: BorderRadius.circular(15)
+                                          ),
+                                        ),
+                                        Container(
+                                          width: 39, height: 19, alignment: Alignment.bottomLeft, margin: EdgeInsets.only(left: 10),
+                                          child: Text('3%', style: TextStyle(color: Color(0xffFF7E47), fontSize: 14),),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(top: 34),
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      width: 68, height: 26, margin: EdgeInsets.only(left: 27,top: 4),
+                                      child: Text('분포', style: TextStyle(color: Color(0xff434343), fontSize: 15, fontWeight: FontWeight.w400),),
+                                    ),
+                                    Container(
+                                      width: 234, height: 26, margin: EdgeInsets.only(left: 37,top: 4), alignment: Alignment.centerRight,
+                                      child: Text('앞윗니에 치태가 몰려있어요', style: TextStyle(color: Color(0xff434343),fontWeight: FontWeight.w600, fontSize: 14),),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(top: 20, left: 35),
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.only(bottom:15),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Row(
+                                            children: [
+                                              Container(
+                                                width: 67, height: 26, alignment: Alignment.centerLeft,
+                                                child: Text('앞니', style: TextStyle( color: Color(0xff9E9E9E), fontSize: 12, fontWeight: FontWeight.w600),),
+                                              ),
+                                              Container(
+                                                width: 101, height: 22, margin: EdgeInsets.only(left: 149), alignment: Alignment.centerRight,
+                                                child: Text('치석 잔여율 5%', style: TextStyle( color: Color(0xffFF7E47), fontSize: 12, fontWeight: FontWeight.w600)),
+                                              )
+                                            ],
+                                          ),
+                                          Container(
+                                            width: 317,height: 137,
+                                            child: ClipRRect(
+                                                borderRadius: BorderRadius.circular(15),
+                                                child: Image.asset("images/reports/teeth/front.png",fit: BoxFit.fitWidth,)
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    Container(
+                                      margin: EdgeInsets.only(bottom:15),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Row(
+                                            children: [
+                                              Container(
+                                                width: 67, height: 26, alignment: Alignment.centerLeft,
+                                                child: Text('아랫니', style: TextStyle( color: Color(0xff9E9E9E), fontSize: 12, fontWeight: FontWeight.w600),),
+                                              ),
+                                              Container(
+                                                width: 101, height: 22, margin: EdgeInsets.only(left: 149), alignment: Alignment.centerRight,
+                                                child: Text('치석 잔여율 12%', style: TextStyle( color: Color(0xffFF7E47), fontSize: 12, fontWeight: FontWeight.w600)),
+                                              )
+                                            ],
+                                          ),
+                                          Container(
+                                            width: 317,height: 137,
+                                            child: ClipRRect(
+                                                borderRadius: BorderRadius.circular(15),
+                                                child: Image.asset("images/reports/teeth/bottom.png",fit: BoxFit.fitWidth,)
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    Container(
+                                      margin: EdgeInsets.only(bottom:15),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Row(
+                                            children: [
+                                              Container(
+                                                width: 67, height: 26, alignment: Alignment.centerLeft,
+                                                child: Text('윗니', style: TextStyle( color: Color(0xff9E9E9E), fontSize: 12, fontWeight: FontWeight.w600),),
+                                              ),
+                                              Container(
+                                                width: 101, height: 22, margin: EdgeInsets.only(left: 149), alignment: Alignment.centerRight,
+                                                child: Text('치석 잔여율 3%', style: TextStyle( color: Color(0xffFF7E47), fontSize: 12, fontWeight: FontWeight.w600)),
+                                              )
+                                            ],
+                                          ),
+                                          Container(
+                                            width: 317,height: 137,
+                                            child: ClipRRect(
+                                                borderRadius: BorderRadius.circular(15),
+                                                child: Image.asset("images/reports/teeth/top.png",fit: BoxFit.fitWidth,)
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    Container(
+
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                width: 77, height: 22, margin: EdgeInsets.only(left: 232, bottom: 50),
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      width: 16, height: 14,
+                                      child: Icon(CupertinoIcons.camera_fill, color: Color(0xff868686), size: 14,),
+                                    ),
+                                    Container(
+                                      margin: EdgeInsets.only(left: 5),
+                                      child: Text('다시 찍기', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600, color: Color(0xff868686)),),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ) :
                         Container(
                           height: 1,
                           margin: EdgeInsets.only(top: 22),
